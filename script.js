d3039c4479d539d2fe6cf5d5f7f8ed6d530803c8
AfanasievAndrey <afanasiev.andrii1983@gmail.com>
2022-10-18 20:27:18 +0200

    Maybe final

--- script.js
+++ script.js
@@ -11,32 +11,59 @@ const computerPlay = function () {
     return "scissors";
   }
 };
-// console.log(computerPlay());
 const playRound = function (playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
   if (playerSelection == computerSelection) {
-    return "It is a draw!!";
-  } else if (playerSelection == "rock") {
-    if (computerSelection == "paper") {
-      return "Computer win";
+    return "It is a draw!";
+  } else if ((playerSelection == "rock") & (computerSelection == "scissor")) {
+    return "Player win";
+  } else if ((playerSelection == "scissor") & (computerSelection == "paper")) {
+    return "Player win";
+  } else if ((playerSelection == "paper") & (computerSelection == "rock")) {
+    return "Player win";
+  } else return "Computer win";
+};
+
+const game = function () {
+  let computerWins = 0,
+    playerWins = 0;
+  for (let i = 1; i <= 5; i++) {
+    let playerSelection = prompt("Choose your move: paper, rock or scissors");
+    const playerSelected = playerSelection.toLowerCase();
+    const computerSelected = computerPlay();
+    if (
+      playerSelected == "rock" ||
+      playerSelected == "paper" ||
+      playerSelected == "scissors"
+    ) {
+      console.log(`Round ${i} \n`);
+      if (playRound(playerSelected, computerSelected) == "It is a draw!") {
+        console.log(`Score - Player: ${playerWins} Computer: ${computerWins}`);
+      } else if (playRound(playerSelected, computerSelected) == "Player win!") {
+        playerWins++;
+        console.log(
+          `Score - Player: ${playerWins} Computer: ${computerWins}\n${playerSelected} vs ${computerSelected}`
+        );
+      } else {
+        computerWins++;
+        console.log(
+          `Score - Player: ${playerWins} Computer: ${computerWins}\n${playerSelected} vs ${computerSelected}`
+        );
+      }
+      console.log(playRound(playerSelected, computerSelected));
     } else {
-      return "Player win this game";
-    }
-  } else if (playerSelection == "scissor") {
-    if (computerSelection == "rock") {
-      return "Computer win";
-    } else {
-      return "Player win this game";
-    }
-  } else if (playerSelection == "paper") {
-    if (computerSelection == "scissors") {
-      return "Computer win";
-    } else {
-      return "Player win this game";
+      alert("Make correct choice: paper, rock or scissors");
+      i--;
     }
   }
+  if (playerWins > computerWins) {
+    console.log("Congrats Player win!");
+    console.log(`Final Score: Player: ${playerWins} Computer: ${computerWins}`);
+  } else if (playerWins < computerWins) {
+    console.log("Computer win!");
+    console.log(`Final Score: Player: ${playerWins} Computer: ${computerWins}`);
+  } else {
+    console.log(`It's a draw!`);
+    console.log(`Final Score: Player: ${playerWins} Computer: ${computerWins}`);
+  }
 };
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-const result = playRound(playerSelection, computerSelection);
-console.log(`${result}, ${playerSelection} vs ${computerSelection}`);
+game();
